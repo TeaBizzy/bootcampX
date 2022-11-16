a7b2413e377cb5b23de20a26eef89063eeb2c59f
Stefan Talbot <satalbot@protonmail.com>
2022-11-16 17:11:13 -0500

    Add Queries & Input Sanitization
    + Added: student.js for testing a query in js with input sanitization.
    + Added: teacher.js for testing a query in js with input sanitization.

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,9 +2,8 @@ const { Pool } = require('pg');
 const fs = require('fs');
 
 const args = process.argv.slice(2);
-const cohortName = args[0];
-const limit = args[1];
-const values = [`%${cohortName}%`, limit]
+const cohort = args[0];
+const values = [`%${cohort}%`]
 
 const q = fs.readFileSync('../0_selects/1_students_without_github.sql').toString();
 
@@ -16,11 +15,13 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
-FROM students
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
-LIMIT $2;
+ORDER BY teachers.name;
 `, values)
 .then(res => {
   console.log(res.rows);
